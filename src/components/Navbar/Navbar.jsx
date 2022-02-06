8fcfb110c2b21a86ad3dcff36fd35cad9cae1201
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-05 23:04:44 -0500

    finish navbar, add fonts and color, and icon logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,32 @@
-import { GoThreeBars } from 'react-icons/go';
+import { useState } from "react";
+import "./navbar.scss";
+import logo from '../img/tiger.png';
 
 const Navbar = () => {
-   return (
-     <>
-       <div className="navbar">
-         <div className="logo">
-           <img src={''} alt="" />
-         </div>
-         <GoThreeBars />
-         <div className="nav-menu">
-           <div className="nav-link">
-             About
-           </div>
-           <div className="nav-link">
-             About
-           </div>
-           <div className="nav-link">
-             About
-           </div>
-           <div className="nav-link">
-             About
-           </div>
-           <div className="nav-btn">
-             <div className="nav-btn-link">
-               Buy Tickets 
-             </div>
-           </div>
-         </div>
-       </div>
-     </>
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <span className="nav-logo">Panaewa Zoo</span>
+        <img src={logo} alt="" className="logo" />
+      </div>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/home">Home</a>
+        <a href="/about">About</a>
+        <a href="/service">Animals</a>
+        <a href="/service">Your Visit</a>
+        <a href="/service">Support us</a>
+        <a href="/contact">Contact</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
   );
 };
+
 export default Navbar;
 
